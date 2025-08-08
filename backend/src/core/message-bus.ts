c6cfa7d4b0a761eff863dc84fa82396b1b5c2a13
tbxwork1 <tonerbox5@gmail.com>
2025-08-08 12:15:36 +0500

    fix: allow sending messages to manager personal chat

--- backend/src/core/message-bus.ts
+++ backend/src/core/message-bus.ts
@@ -1,16 +1,28 @@
-// core/message-bus.ts
 import { sendTelegramMessage } from "../adapters/telegram/telegram-adapter";
 import { sendWhatsAppMessage } from "../adapters/whatsapp/whatsapp-adapter";
 import { getChat, ChatPlatform } from "../store/chatStore";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function sendMessageToClient(
   platform: ChatPlatform,
   chatId: string,
   text: string
 ): Promise<void> {
+  const WA_MANAGER_NUMBER = process.env.WA_MANAGER_NUMBER;
+
+  console.log(platform, chatId, text);
+
+  if (platform === "whatsapp" && chatId === WA_MANAGER_NUMBER) {
+    return await sendWhatsAppMessage(chatId, text);
+  }
+
   const chat = getChat(platform, chatId);
   if (!chat) throw new Error("Chat not found");
+
   console.log("🚨 Платформа:", platform);
+
   if (platform === "telegram") {
     return await sendTelegramMessage(chatId, text);
   }
